99f06a8c183e3d645c3aa45d8857a0e723a3b338
ella-chetvertak <olegcetvertak037@gmail.com>
2022-06-07 21:34:57 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+alert('Hello World')
+let random_number = 123456
+let school_name = "IT School 'Hillel'"
+let bool = false
+console.log(random_number, school_name, bool)
+console.log(typeof random_number, typeof school_name, typeof bool)
